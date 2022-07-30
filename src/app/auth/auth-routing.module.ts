8f390673594965d7f96c07b41cb0fabe34f74b3a
Winser Espinal <winser500@gmail.com>
2022-07-30 01:08:46 -0400

    login feature module added

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'forgot', loadChildren: () => import('./forgot/forgot.module').then(m => m.ForgotModule) }];
+const routes: Routes = [{ path: 'forgot', loadChildren: () => import('./forgot/forgot.module').then(m => m.ForgotModule) }, { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
