183299992ce0e03a4d5d1a7c534a539c0b87f641
Winser Espinal <winser500@gmail.com>
2022-07-30 00:23:02 -0400

    Features module creado: ng g m [name] --route [folder name within route] --routing --module [parent module]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'features', loadChildren: () => import('./features/features.module').then(m => m.FeaturesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
