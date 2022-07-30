cfbe6ab7d3ed6de3687ef23a58f1d997d1f888e0
Winser Espinal <winser500@gmail.com>
2022-07-30 00:29:13 -0400

    Add feature module added to feature:ng g m features/add --route add --routing --module features.module

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FeaturesComponent } from './features.component';
 
-const routes: Routes = [{ path: '', component: FeaturesComponent }];
+const routes: Routes = [{ path: '', component: FeaturesComponent }, { path: 'add', loadChildren: () => import('./add/add.module').then(m => m.AddModule) }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
